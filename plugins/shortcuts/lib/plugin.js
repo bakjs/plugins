d9cc60d48990825b184411b866ba11b9a2d77476
Pooya Parsa <pooya@pi0.ir>
2017-10-19 14:26:13 +0330

    feat: new plugins from core

---
+++ plugins/shortcuts/lib/plugin.js
@@ -0,0 +1,13 @@
+const { realIP } = require('../utils')
+
+exports.register = function bakShortcuts (server, config) {
+  server.ext('onPreHandler', (request, h) => {
+    request.user = request.auth.credentials ? request.auth.credentials.user : null
+    request.session = request.auth.artifacts
+    request.ip = realIP(request)
+
+    return h.continue
+  })
+}
+
+exports.pkg = require('..')
