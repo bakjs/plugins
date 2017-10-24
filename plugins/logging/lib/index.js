c0f2d728f09c7084667fb446d4bbe4a6f9c5f5c5
Pooya Parsa <pooya@pi0.ir>
2017-10-24 23:39:41 +0330

    feat(logging): remvoe dependency from bak

--- plugins/logging/lib/index.js
+++ plugins/logging/lib/index.js
@@ -1,10 +1,12 @@
-const { Errors } = require('bak')
+const { error } = require('./errors')
 
 exports.register = function (server, options) {
   server.events.on({ name: 'request', channels: 'internal' }, (request, { timestamp, error }, tags) => {
     // const tagsStr = '[' + Object.keys(tags).join('][') + ']'
-    Errors.error(error)
+    error(error)
   })
 }
 
 exports.pkg = require('../package.json')
+
+exports.error = error
