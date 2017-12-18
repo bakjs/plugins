30573ba22da7cfa8181991ba3141d53d21742fe9
Pooya Parsa <pooya@pi0.ir>
2017-12-18 19:03:18 +0330

    feat(auth): Rewrite for bak 3.x
    BREAKING CHANGE: requires bak >= 3

--- plugins/auth/lib/controller.js
+++ plugins/auth/lib/controller.js
@@ -2,56 +2,49 @@ const { Controller } = require('bak')
 
 class $AuthController extends Controller {
   constructor (authProvider) {
-    super({
-      prefix: '/api',
-      default: {
-        auth: false
-      },
-      routes: {
-        auth_user: {
-          auth: { mode: 'required' }
-        },
-        auth_login_post: {
-          enabled: authProvider.loginSupported
-        },
-        auth_logout: {
-          auth: { mode: 'required' },
-          enabled: authProvider.loginSupported
-        },
-        oauth_$clientID_login: {
-          enabled: authProvider.oauthSupported
-        },
-        oauth_$clientID_authorize: {
-          enabled: authProvider.oauthSupported
-        }
-      }
-    })
-
+    super()
     this.authProvider = authProvider
   }
 
-  async auth_login_post (request, h) {
+  init () {
+    this.prefix = '/api'
+    this.defaults.auth = false
+
+    this.get('/auth/user', this.user, {auth: {mode: 'required'}})
+
+    if (this.authProvider.loginSupported) {
+      this.post('/auth/login', this.login)
+      this.post('/auth/logout', this.logout, {auth: {mode: 'required'}})
+    }
+
+    if (this.authProvider.oauthSupported) {
+      this.any('/oauth/{clientID}/login', this.oauthLogin)
+      this.any('/oauth/{clientID}/authorize', this.oauthAuthorize)
+    }
+  }
+
+  async login (request, h) {
     let { username, password } = request.payload || {}
     let { token } = await this.authProvider.login({ username, password, request })
     return { token }
   }
 
-  async auth_logout (request, h) {
+  async logout (request, h) {
     let { session, user } = request
     await this.authProvider.logout({ user, session })
     return 'LOGGED_OUT'
   }
 
-  async auth_user (request, h) {
+  async user (request, h) {
     return { user: request.user }
   }
 
-  async oauth_$clientID_login_any (request, h) {
+  async oauthLogin (request, h) {
     let redirect_uri = await this.authProvider.oauthLogin(request.params.clientID)
     return { redirect_uri }
   }
 
-  async oauth_$clientID_authorize_any (request, h) {
+  async oauthAuthorize (request, h) {
     let { token, user } = await this.authProvider.oauthAuthorize(request.params.clientID, request)
     return { token, user }
   }
