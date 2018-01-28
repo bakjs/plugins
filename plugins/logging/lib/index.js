eafe4678e0de7cd4479f66083097367fdd717edb
Pooya Parsa <pooya@pi0.ir>
2018-01-29 02:58:41 +0330

    feat(logging): better Boom error handling

--- plugins/logging/lib/index.js
+++ plugins/logging/lib/index.js
@@ -1,15 +1,35 @@
+const Chalk = require('chalk')
 const Errors = require('./errors')
 
 exports.register = function (server, options) {
   const isDev = process.env.NODE_ENV !== 'production'
 
-  server.events.on({ name: 'request', channels: 'internal' }, (request, { timestamp, error }, tags) => {
-    if (!error || (!isDev && error.isBoom)) {
-      return
-    }
+  server.events.on(
+    { name: 'request', channels: 'internal' },
+    (request, { timestamp, error }, tags) => {
+      if (!error) {
+        return
+      }
 
-    Errors.error(error)
-  })
+      if (error.isBoom) {
+        let { name, message, statusCode } = error
+
+        if (error.output) {
+          message = message || error.output.payload.message
+          statusCode = error.output.payload.statusCode
+        }
+
+        console.error(
+          '\n' + Chalk.red(`[${name}] [${statusCode}]`),
+          Chalk.red(request.path),
+          Chalk.grey(`\n${message}`)
+        )
+        return
+      }
+
+      Errors.error(error)
+    }
+  )
 }
 
 exports.once = true
